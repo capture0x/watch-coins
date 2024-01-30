621af3e00f9317211f87b215f51518071a81e16f
capture0x <61238348+capture0x@users.noreply.github.com>
2024-01-30 15:30:32 +0000

    Update and rename aa.sh to setup.sh

--- aa.sh
+++
@@ -1,5 +0,0 @@
-wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-sudo mv chromedriver /usr/bin/chromedriver
-sudo chown root:root /usr/bin/chromedriver
-sudo chmod +x /usr/bin/chromedriver
---
+++ setup.sh
@@ -0,0 +1,5 @@
+wget https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/120.0.6099.109/linux64/chromedriver-linux64.zip
+unzip chromedriver-linux64.zip
+sudo mv chromedriver /usr/bin/chromedriver
+sudo chown root:root /usr/bin/chromedriver
+sudo chmod +x /usr/bin/chromedriver
